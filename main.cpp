f78f48de3d31c02a261169712b9102b1bf5889bd
Adolfo Gante <adolfogante@gmail.com>
2023-11-19 00:55:37 -0800

    updated help()

--- main.cpp
+++ main.cpp
@@ -110,6 +110,8 @@ int main (int argc, char *argv[]) {
 	return 0;
 }
 
+// Functions
+
 void timer(int time) {
 	int counter = 60 * time; //amount of seconds
 
@@ -163,11 +165,17 @@ void remove(int id) {
 }
 
 void help() {
-	std::cout << "add   : takes a string input and appends the task list file." << std::endl;
-	std::cout << "remove: takes the task index integer and removes the task from task list file." << std::endl;
-	std::cout << "print : takes no input and prints all tasks with thier index." << std::endl;
-	std::cout << "clear : takes no input and removes all tasks from task list file." << std::endl;
-	std::cout << "help  : takes no input and removes all tasks from task list file." << std::endl;
+	std::cout << "add    : takes a string input and appends the task list." << std::endl;
+	std::cout << "remove : takes the task index integer and removes the task from task list." << std::endl;
+	std::cout << "print  : takes no input and prints all tasks with thier index." << std::endl;
+	std::cout << "clear  : takes no input and removes all tasks from task list file." << std::endl;
+	std::cout << "help   : takes no input and prints this help text." << std::endl;
+	std::cout << "restore: takes no input and sets default pomodoro values." << std::endl;
+	std::cout << "-n flag for number of pomodoros." << std::endl;
+	std::cout << "-p flag for pomodoro time length." << std::endl;
+	std::cout << "-s flag for short break length." << std::endl;
+	std::cout << "-l flag for long break length." << std::endl;
+	std::cout << "-i flag for long break intervals." << std::endl;
 }
 
 void print() {
