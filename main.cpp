560f5f1dc729bd47beed86c93a178be2592aec21
adoante <adolfogante@gmail.com>
2023-11-18 16:20:10 -0800

    get some, whooooo

--- main.cpp
+++ main.cpp
@@ -1,153 +1,222 @@
 #include <iostream>
-#include <chrono>
-#include <unistd.h>
-#include <list>
 #include <string.h>
 #include <fstream>
 #include <vector>
-#include <conio.h>
-#include <thread>
-
 #include <unistd.h>
+#include <windows.h>
 
-using namespace std::chrono;
+void timer(int);
+void remove(int);
+void help();
+void clear();
+void print();
+void add(std::string);
+void load_config(int&, int&, int&, int&, int&);
+void restore();
 
 int main (int argc, char *argv[]) {
 
-	int n = 1;
-	int i = 4;
-	int p = 25;
-	int s = 5;
-	int l = 15;
+	int n = 0;	// number of pomodoros
+	int i = 0;	// interval of long breaks
+	int p = 0;	// length of pomodoro
+	int s = 0;	// length of short break
+	int l = 0;	// length of long break
+
+	load_config(n, i, p, s, l);
+
+	int opt;
+	
+	// allows user to set config
+	while ((opt = getopt(argc, argv, "n:p:s:l:i:")) != -1) {
+	    switch (opt) {
+			case ('n'):
+				n = atoi(optarg);
+				std::cout << "Set number of pomodoros: " << n << std::endl;
+				break;
+			case ('p'):
+				p = atoi(optarg);
+				std::cout << "Set length of pomodoro: " << p << std::endl;
+				break;
+			case ('s'):
+				s = atoi(optarg);
+				std::cout << "Set length of short break: " << s << std::endl;
+				break;
+			case ('l'):
+				l = atoi(optarg);
+				std::cout << "Set length of long break: " << l << std::endl;
+				break;
+			case ('i'):
+				i = atoi(optarg);
+				std::cout << "Set interval of breaks: " << i << std::endl;
+				break;
+		}
+	}
 
 	// add task to list
 	if (strcmp(argv[1], "add") == 0) {
-		std::ofstream myfile;
-		myfile.open("tasks.txt", std::ios_base::app);
-		myfile << argv[2] << std::endl;
-		myfile.close();
+		add(argv[2]);
 	}
 
 	//print task list
 	if (strcmp(argv[1], "print") == 0) {
-		std::ifstream infile("tasks.txt");
-		std::string line;
-		int index = 0;
-		while (std::getline(infile, line)) {
-			std::cout << "[" << index  << "]: " << line << std::endl;
-			index++;
-		}
+		print();
 	}
 
 	//clear all task from list
 	if (strcmp(argv[1], "clear") == 0) {
-		std::ofstream myfile;
-		myfile.open("tasks.txt");
-		myfile.close();
-		std::cout << "All tasks were removed." << std::endl;
+		clear();
 	}
 
 	//remove task from list
 	if (strcmp(argv[1], "remove") == 0) {
-		std::ifstream infile("tasks.txt");
-		std::string line;
-
-		std::vector<std::string> tasks;
-		int index = atoi(argv[2]);
-
-		while (std::getline(infile, line)) {
-			tasks.push_back(line);
-		}
-
-		std::cout << "Removed task [" << index << "]: " << tasks[index] << std::endl;
-		tasks[index] = "";
-
-		std::ofstream myfile;
-		myfile.open("tasks.txt");
-		for (int i = 0; i < tasks.size(); i++) {
-			if (tasks[i] != "") {
-				myfile << tasks[i] << std::endl;
-			}
-		}
-		myfile.close();
+		remove(atoi(argv[2]));
 	}
 
 	//print help page with command and flag
 	if (strcmp(argv[1], "help") == 0) {
-		std::cout << "add: takes a string input and appends the task list file." << std::endl;
-		std::cout << "remove: takes the task index integer and removes the task from task list file." << std::endl;
-		std::cout << "print: takes no input and prints all tasks with thier index." << std::endl;
-		std::cout << "clear: takes no input and removes all tasks from task list file." << std::endl;
+		help();
+	}
+
+	//restores config to default
+	if (strcmp(argv[1], "restore") == 0) {
+		restore();
 	}
 
 	//start pomodoro or a specific task
 	if (strcmp(argv[1], "start") == 0) {
+		int interval_count = 0;
 		if (argc == 3) {
-			int index = atoi(argv[2]);
+			std::cout << "Starting task: " << argv[2] << std::endl;
+			for (int j = 0; j < n; j++) {
+				timer(p);
+				interval_count++;
+
+				if (interval_count % i == 0) {
+					timer(i);
+					interval_count++;
+				}
+				else {
+					timer(s);
+					interval_count++;
+				}
+			}
+			remove(atoi(argv[2]));
 		}
 		else {
-			auto now = std::chrono::system_clock::now();
-			auto end = now + minutes(p);
-			std::time_t t_c1 = std::chrono::system_clock::to_time_t(end);
-			std::cout << "The system clock is end at " << std::ctime(&t_c1);
-			while (true) {
-				now = std::chrono::system_clock::now();
-				std::time_t t_c2 = std::chrono::system_clock::to_time_t(now);
-				if (t_c1 == t_c2) {
-					break;
-				}
-			    std::cout << "The system clock is currently at " << std::ctime(&t_c2);
-				std::this_thread::sleep_for(seconds(1));
-				system("@cls||clear");
-			}
-			std::cout << "Pomodoro Finished! " << std::endl;
+			timer(p);
 		}
 	}
 
 	return 0;
 }
 
-/**
- * 
- int id = 0;
-	int n = 1;
-	int i = 4;
-	int p = 0;
-	int s = 0;
-	int l = 0;
-	int opt;
+void timer(int time) {
+	int counter = 60 * time; //amount of seconds
 
-  while ((opt = getopt(argc, argv, "n:p:s:l:i:")) != -1) {
-	    switch (opt) {
-			case ('n'):
-				n = atoi(optarg);
-				break;
-			case ('p'):
-				p = atoi(optarg);
-				break;
-			case ('s'):
-				s = atoi(optarg);
-				break;
-			case ('l'):
-				l = atoi(optarg);
-				break;
-			case ('i'):
-				i = atoi(optarg);
-				break;
+		int min = time - 1;
+		int sec = 59;
+
+	    Sleep(1000);
+	    while (counter >= 1)
+	    {
+	        if (sec < 10) {
+				std::cout << "\rTime remaining: " << min << ":0" << sec  << std::flush;
+			}
+			else {
+				std::cout << "\rTime remaining: " << min << ":" << sec  << std::flush;
+			}
+	        Sleep(1000);
+			sec--;
+	        counter--;
+			if (sec == 0) {
+				min--;
+				sec = 59;
+			}
+	    }
+		min = 0;
+		sec = 0;
+		std::cout << "\rTime remaining: " << min << ":" << sec  << std::flush;
+}
+
+void remove(int id) {
+	std::ifstream infile("tasks.txt");
+	std::string line;
+
+	std::vector<std::string> tasks;
+	int index = id;
+
+	while (std::getline(infile, line)) {
+		tasks.push_back(line);
+	}
+
+	std::cout << "Removed task [" << index << "]: " << tasks[index] << std::endl;
+	tasks[index] = "";
+
+	std::ofstream myfile;
+	myfile.open("tasks.txt");
+	for (int i = 0; i < tasks.size(); i++) {
+		if (tasks[i] != "") {
+			myfile << tasks[i] << std::endl;
 		}
 	}
+	myfile.close();
+}
 
-	
+void help() {
+	std::cout << "add   : takes a string input and appends the task list file." << std::endl;
+	std::cout << "remove: takes the task index integer and removes the task from task list file." << std::endl;
+	std::cout << "print : takes no input and prints all tasks with thier index." << std::endl;
+	std::cout << "clear : takes no input and removes all tasks from task list file." << std::endl;
+	std::cout << "help  : takes no input and removes all tasks from task list file." << std::endl;
+}
 
-	
-
-	//start pomodoro or a specific task
-	if (strcmp(argv[1], "start") == 0) {
-
+void print() {
+	std::ifstream infile("tasks.txt");
+	std::string line;
+	int index = 0;
+	while (std::getline(infile, line)) {
+		std::cout << "[" << index  << "]: " << line << std::endl;
+		index++;
 	}
+}
 
-	//end pomodoro
-	if (strcmp(argv[1], "end") == 0) {
+void clear() {
+	std::ofstream myfile;
+	myfile.open("tasks.txt");
+	myfile.close();
+	std::cout << "All tasks were removed." << std::endl;
+}
 
-	}
- */
+void add(std::string task) {
+	std::ofstream myfile;
+	myfile.open("tasks.txt", std::ios_base::app);
+	myfile << task << std::endl;
+	myfile.close();
+}
+
+void load_config(int &n, int &i, int &p, int &s, int &l) {
+	// load default config
+	std::ifstream infile("config.txt");
+	std::string line;
+	std::getline(infile, line);
+	n = stoi(line);
+	std::getline(infile, line);
+	i = stoi(line);
+	std::getline(infile, line);
+	p = stoi(line);
+	std::getline(infile, line);
+	s = stoi(line);
+	std::getline(infile, line);
+	l = stoi(line);
+}
+
+void restore() {
+	std::ofstream myfile;
+	myfile.open("config.txt");
+	myfile << "1" << std::endl;
+	myfile << "4" << std::endl;
+	myfile << "25" << std::endl;
+	myfile << "5" << std::endl;
+	myfile << "15" << std::endl;
+	myfile.close();
+}
